005d7e5be442f43da32beb55c2876de7bcbf4805
omar <opm@hotmail.com>
2018-08-28 22:23:24 +0100

    Sync data with the UI

--- js/vimeo.js
+++ js/vimeo.js
@@ -5,10 +5,32 @@ let vimeoApp = (function(window){
   let buttonFilter = document.querySelector(".js-button-filter");
   let inputMaxResult = document.querySelector(".js-max-result");
   let inputMoreThan = document.querySelector(".js-more-than");
+  let container = document.querySelector(".l_main-content");
 
   function render()
   {
     console.log(stateManagaer.getState())
+
+    let rendredView = vimeoData.data.map(function(video){
+
+      return `<div class="card">
+              <img src="${video.user.pictures.sizes[4].link}" alt=""
+                class="card__img">
+              <div class="card__content">
+                <h2 class="card__title">${video.name}</h2>
+                <p class="card__description">${video.description && video.description.substr(0,150)}${ video.description && video.description.length > 150 ? "..." : "" }</p>
+                <ul class="card__metadata">
+                  <li>Plays: ${video.stats.plays}</li>
+                  <li>Comments: ${video.metadata.connections.comments.total}</li>
+                  <li>Likes: ${video.metadata.connections.likes.total}</li>
+                </ul>
+              </div>
+            </div>` 
+
+    }).join("");
+
+    container.innerHTML = rendredView;
+
   }
 
   function bindEvents(){
