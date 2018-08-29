6890c91d115db543561c63f102d1dbfc4f3c0917
ochajia <ochajia@sqli.com>
2018-08-29 18:39:06 +0100

    functional navigation

--- js/vimeo.js
+++ js/vimeo.js
@@ -5,6 +5,10 @@ let vimeoApp = (function (window) {
   let inputMaxResult = document.querySelector('.js-max-result')
   let inputMoreThan = document.querySelector('.js-more-than')
   let container = document.querySelector('.l_main-content')
+  let prevBtn = document.querySelector('.js-prev-btn')
+  let nextBtn = document.querySelector('.js-next-btn')
+
+  let filtredResult;
 
   function init () {
     bindEvents()
@@ -15,44 +19,12 @@ let vimeoApp = (function (window) {
       filter: '',
       withMoreLikes: false,
       maxResults: 10,
-      page: 1
+      page: 0,
     })
   }
 
-  function render () {
-    let rendredView = vimeoData.data
-      .filter((video)=>{
-        return (
-          (video.description &&
-            video.description.includes(stateManager.getState().filter)) ||
-          stateManager.getState().filter === ''
-        )
-      })
-      .filter((video)=>{
-        return stateManager.getState().widthMoreLikes ? video.user.metadata.connections.likes.total > 10 : true;
-      })
-      .slice(0, stateManager.getState().maxResults)
-      .map((video)=>{
-        return `<div class="card">
-              <a href="${video.user.link}"><img src="${video.user.pictures.sizes[4].link}" alt="profile"
-                class="card__img"></a>
-              <div class="card__content">
-              <h2 class="card__title"><a href="${video.link}">${video.name}</a></h2>
-                <p class="card__description">${video.description && video.description.substr(0, 150)}${video.description && video.description.length > 150 ? '...' : ''}</p>
-                <ul class="card__metadata">
-                  <li><i class="fas fa-play"></i>  ${video.stats.plays}</li>
-                  <li><i class="fas fa-comment"></i>  ${video.metadata.connections.comments.total}</li>
-                  <li><i class="fas fa-heart"></i>  ${video.metadata.connections.likes.total}</li>
-                </ul>
-              </div>
-            </div>`
-      })
-      .join('')
-
-    container.innerHTML = rendredView
-  }
-
   function bindEvents () {
+
     buttonFilter.addEventListener('click', function () {
       stateManager.updateState({
         ...stateManager.getState(),
@@ -75,6 +47,86 @@ let vimeoApp = (function (window) {
         widthMoreLikes: this.checked
       })
     })
+
+    prevBtn.addEventListener("click",function(e){
+      e.preventDefault();
+
+      stateManager.updateState({
+        ...stateManager.getState(),
+        page: stateManager.getState().page - 1
+      })
+
+    })
+    nextBtn.addEventListener("click",function(e){
+      e.preventDefault();
+
+      stateManager.updateState({
+        ...stateManager.getState(),
+        page: stateManager.getState().page + 1
+      })
+
+    })
+
+  }
+
+  function render () {
+    filtredResult = vimeoData.data
+      .filter(video => {
+        return (
+          (video.description &&
+            video.description.includes(stateManager.getState().filter)) ||
+          stateManager.getState().filter === ''
+        )
+      })
+      .filter(video => {
+        return stateManager.getState().widthMoreLikes
+          ? video.user.metadata.connections.likes.total > 10
+          : true
+      });
+
+      let rendredView = filtredResult.slice( stateManager.getState().page * stateManager.getState().maxResults , stateManager.getState().page * stateManager.getState().maxResults +  stateManager.getState().maxResults)
+      .map(video => {
+        return `<div class="card">
+              <a href="${video.user.link}"><img src="${video.user.pictures.sizes[4].link}" alt="profile"
+                class="card__img"></a>
+              <div class="card__content">
+              <h2 class="card__title"><a href="${video.link}">${video.name}</a></h2>
+                <p class="card__description">${video.description && video.description.substr(0, 150)}${video.description && video.description.length > 150 ? '...' : ''}</p>
+                <ul class="card__metadata">
+                  <li><i class="fas fa-play"></i>  ${video.stats.plays}</li>
+                  <li><i class="fas fa-comment"></i>  ${video.metadata.connections.comments.total}</li>
+                  <li><i class="fas fa-heart"></i>  ${video.metadata.connections.likes.total}</li>
+                </ul>
+              </div>
+            </div>`
+      })
+
+    container.innerHTML = rendredView.join('')
+
+    console.log(stateManager.getState())
+
+    updateNavigationButtonsStatus();
+
+  }
+
+  function updateNavigationButtonsStatus()
+  {
+    if(stateManager.getState().page <= 0)
+    {
+      prevBtn.classList.add("disabled");
+    }else{
+      prevBtn.classList.remove("disabled");
+    }
+    if( Math.ceil(filtredResult.length / stateManager.getState().maxResults) - 1 <= stateManager.getState().page)
+    {
+      nextBtn.classList.add("disabled");
+    }else{
+      nextBtn.classList.remove("disabled");
+    }
+
+    // console.log(Math.ceil(filtredResult.length / stateManager.getState().maxResults));
+    // console.log(stateManager.getState().page);
+
   }
 
   return {
